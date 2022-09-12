cbf881f29eb3a71dc8ad5ba6ab4503b9447513c6
CQI8 <manoel.dison@petrobras.com.br>
2022-09-12 18:31:25 -0300

    First working version

---
+++ src/compjobs.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+from json import loads
+from subprocess import run, PIPE, DEVNULL
+from sys import argv
+from typing import Dict
+
+from dictionariescomparator import DictionariesComparator
+
+
+def runshell(cmd: str, verbose=False) -> None:
+    """
+    Returns the stdout of shell command passed in cmd argument.
+    Prints stdout if verbose=True
+    """
+    if cmd:
+        if '\n' in cmd:
+            results = [
+                runshell(line.strip()) 
+                for line in cmd.split('\n') if line.strip()
+                ]
+            return results[-1]
+        else:
+            tokens = cmd.split(' ')
+            result = run(tokens, stdout=PIPE, stderr=DEVNULL).stdout
+            if verbose:
+                print(result)
+            return result
+
+
+def get_extra_vars(job: int) -> Dict:
+    """
+    Returs the extra_vars of passed job as a dictionary.
+    """
+    cmd_res = runshell(f'awx-cli job get {job} -f json')
+    job_details = loads(cmd_res)['extra_vars']
+    extra_vars = loads(job_details)
+    return extra_vars
+
+
+def compare_jobs(job_a: int, job_b: int) -> None:
+    """
+    Performs a comparison between the extra_vars of the jobs passed.
+    """
+    extra_vars_a = get_extra_vars(job_a)
+    extra_vars_b = get_extra_vars(job_b)
+    DictionariesComparator(extra_vars_a, extra_vars_b, job_a, job_b)
+
+
+def main():
+    if len(argv) == 3:
+        compare_jobs(argv[1], argv[2])
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/dictionariescomparator.py
@@ -0,0 +1,86 @@
+from typing import Dict, List
+
+
+class DictionariesComparator:
+    """
+    Given two dictionaries, reports:
+    - which keys have the same value in both dictonaries,
+    - which are present in both dictionaries, but with different values,
+    - which are unique.
+    """
+    def __init__(self, a: Dict, b: Dict, a_id: int, b_id: int, all_groups: bool = False) -> None:
+        self.a = a
+        self.b = b
+        self.a_id = a_id
+        self.b_id = b_id
+        self.all_groups = all_groups
+        self.__distribute_keys()
+        self.__print('Same key and value', self.same_value)
+        self.__print('Same key', self.same_key)
+        self.__print('Unique keys', self.unique_keys)
+
+    def __distribute_keys(self) -> None:
+        """
+        Merges the keys of both dicitionaries and group them in three
+        groups: same_value, same_key and unique_keys.
+        """
+        self.same_value = []
+        self.same_key = []
+        self.unique_keys = []
+        merged = {**self.a, **self.b}
+        all_keys = sorted(merged.keys())
+        for k in all_keys:
+            if k in self.a and k in self.b:
+                if self.a[k] == self.b[k]:
+                    self.same_value.append(k)
+                else:
+                    self.same_key.append(k)
+            else:
+                self.unique_keys.append(k)
+    
+    def __print(self, title: str, group: List) -> None:
+        """
+        Prints the keys and its values in each dicionary that belong to 
+        the passed group.
+        """
+        KEYSIZE = 30
+        VALUESIZE = 50
+
+        def make_line(key: str, a: str, b: str) -> str:
+            """
+            Returns a line composed by a key and its value in each dictionary.
+            """
+            line = f'{key:<{KEYSIZE}} {a:<{VALUESIZE}} {b:<{VALUESIZE}}'
+            return line
+
+        def get_value_into_pieces(dic: Dict, key: str) -> List:
+            """
+            Splits the string representation of the key's value, whenever its
+            length is bigger than VALUESIZE.
+            """
+            value = str(dic.get(key, ''))
+            pieces = [value]
+            if len(value) > VALUESIZE:
+                pieces = []
+                while value:
+                    pieces.append(value[:VALUESIZE])
+                    value = value[VALUESIZE:]
+            return pieces
+
+        if group or self.all_groups:
+            header = make_line(title, self.a_id, self.b_id)
+            dashed_line = len(header) * '-'
+            print()
+            print(header)
+            print(dashed_line)
+            for k in group:
+                pieces_a = get_value_into_pieces(self.a, k)
+                pieces_b = get_value_into_pieces(self.b, k)
+                key_line = make_line(k, pieces_a.pop(0), pieces_b.pop(0))
+                print(key_line)
+                while pieces_a or pieces_b:
+                    piece_a = pieces_a.pop(0) if pieces_a else ''
+                    piece_b = pieces_b.pop(0) if pieces_b else ''
+                    continuation_line = make_line('', piece_a, piece_b)
+                    print(continuation_line)
+            print()
